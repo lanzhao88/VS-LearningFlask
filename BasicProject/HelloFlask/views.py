87c37ab838feb7b9ae08e6b15e87a93b51c938d8
lanzhao88 <lanzhao88@gmail.com>
2018-09-14 09:08:47 -0400

    PAge navigation support.

--- BasicProject/HelloFlask/views.py
+++ BasicProject/HelloFlask/views.py
@@ -7,9 +7,20 @@ from HelloFlask import app
 @app.route('/home')
 def home():
     now = datetime.now()
-    formatted_now = now.strftime("%A, %d %BaseException, %Y at %X")
+    formatted_now = now.strftime("%A, %d %B, %Y at %X")
 
     return render_template(
         "index.html", title = "Hello Flask",
         message = "Hello, Flask!",
         content = " on " + formatted_now)
+
+@app.route('/api/data')
+def get_data():
+    return app.send_static_file('data.json')
+
+@app.route('/about')
+def about():
+    return render_template(
+        "about.html",
+        title="About HeooFlask",
+        content = "Example app page for Flask.:) ")
